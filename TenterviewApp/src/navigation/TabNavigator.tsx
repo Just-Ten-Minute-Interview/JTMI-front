c19af1c82c017b8b21c664cdc837b48d0cd515c3
OH-JOO-YEONG <brb1111@naver.com>
2022-07-27 16:34:49 +0900

    [Chore & Feat] module import alias 설정 및 리액트 네비게이션 기능 적용

---
+++ TenterviewApp/src/navigation/TabNavigator.tsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from '@/screens/HomeScreen';
+import BookmarkScreen from '@/screens/BookmarkScreen';
+import SettingScreen from '@/screens/SettingScreen';
+import HistoryScreen from '@/screens/HistoryScreen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: 15,
+            left: 15,
+            right: 15,
+            backgroundColor: '#ffffff',
+            borderRadius: 30,
+            height: 70,
+            ...styles.shadow,
+          },
+          headerShown: false,
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.iconContainer}>
+                <Ionicons
+                  name="home-sharp"
+                  size={25}
+                  style={{
+                    marginBottom: 5,
+                  }}
+                  color={focused ? '#12B886' : '#748c94'}
+                />
+                <Text
+                  style={{
+                    color: focused ? '#12B886' : '#748c94',
+                    fontSize: 12,
+                  }}>
+                  홈
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Bookmark"
+          component={BookmarkScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.iconContainer}>
+                <Ionicons
+                  name="bookmark"
+                  size={25}
+                  style={{
+                    marginBottom: 5,
+                  }}
+                  color={focused ? '#12B886' : '#748c94'}
+                />
+
+                <Text
+                  style={{
+                    color: focused ? '#12B886' : '#748c94',
+                    fontSize: 12,
+                  }}>
+                  북마크
+                </Text>
+              </View>
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="History"
+          component={HistoryScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.iconContainer}>
+                <FontAwesome
+                  name="history"
+                  size={25}
+                  style={{
+                    marginBottom: 5,
+                  }}
+                  color={focused ? '#12B886' : '#748c94'}
+                />
+
+                <Text
+                  style={{
+                    color: focused ? '#12B886' : '#748c94',
+                    fontSize: 12,
+                  }}>
+                  히스토리
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Setting"
+          component={SettingScreen}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View style={styles.iconContainer}>
+                <Ionicons
+                  name="settings"
+                  size={25}
+                  style={{
+                    marginBottom: 5,
+                  }}
+                  color={focused ? '#12B886' : '#748c94'}
+                />
+                <Text
+                  style={{
+                    color: focused ? '#12B886' : '#748c94',
+                    fontSize: 12,
+                  }}>
+                  설정
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#7f5df0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+  iconContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: 5,
+  },
+});
+
+export default TabNavigator;
---
+++ TenterviewApp/src/screens/HistoryScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HistoryScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>History Screen</Text>
+    </View>
+  );
+};
+
+export default HistoryScreen;
